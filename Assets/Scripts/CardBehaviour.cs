a5f9c2af4ee675946eb24836880c943162442c3d
Tudor230 <133231902+Tudor230@users.noreply.github.com>
2024-10-06 17:52:50 +0300

    Add player name

--- Assets/Scripts/CardBehaviour.cs
+++ Assets/Scripts/CardBehaviour.cs
@@ -1,24 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;  // Needed for Player class and ownership callbacks
 
-public class CardBehaviour : MonoBehaviour
+public class CardBehaviour : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 offset;
+    private bool isDragging = false;
+
+    private void Start()
     {
-        
+        PhotonNetwork.AddCallbackTarget(this); // Register for ownership callbacks
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        PhotonNetwork.RemoveCallbackTarget(this); // Unregister ownership callbacks
     }
+
+    private void OnMouseDown()
+    {
+        if (!photonView.IsMine)
+        {
+            // If the player doesn't own the card, request ownership
+            photonView.RequestOwnership();
+        }
+        else
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            offset = transform.position - mousePos;
+        }
+    }
+
     private void OnMouseDrag()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        transform.position = mousePos;
+        if (photonView.IsMine) // Only allow movement if this player owns the card
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            transform.position = mousePos + offset;
+        }
+    }
+
+    // Handle the ownership request
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        if (targetView == photonView)
+        {
+            Debug.Log("Ownership requested by: " + requestingPlayer.NickName);
+            // Optionally, handle ownership request approval logic here
+        }
+    }
+
+    // Callback when ownership is transferred
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        if (targetView == photonView)
+        {
+            Debug.Log("Ownership transferred to: " + photonView.Owner.NickName);
+        }
+    }
+
+    // Handle failed ownership transfer
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        if (targetView == photonView)
+        {
+            Debug.Log("Ownership transfer failed for: " + photonView.Owner.NickName);
+        }
     }
 }
